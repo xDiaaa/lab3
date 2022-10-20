6601f665f873f6046087813275e0044dddc9e5e5
xDiaaa <115627169+xDiaaa@users.noreply.github.com>
2022-10-20 09:38:59 +0300

    Update locatie.js

--- locatie.js
+++ locatie.js
@@ -6,7 +6,7 @@ function on_success(position){
   document.getElementById("acc").innerHTML = "Accuracy   = "+position.coords.accuracy;
   document.getElementById("altitude").innerHTML = "Altitude  = "+position.coords.altitude;
   var   latlon   =   position.coords.latitude   +   ","   + position.coords.longitude;
-  var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyBcN6GeMRdibWjmYD4DXVnAbFuovV5jIX0";
+  var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyB33FbzUF2wkEJCr0DhK5x0FS0GrGSXlmM";
   document.getElementById("map").innerHTML = "<img src='"+img_url+"'>";
 }
 function on_error(e){
